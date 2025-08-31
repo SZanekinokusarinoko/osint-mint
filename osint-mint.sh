0496f709e737cc4c7fafad6c5a5c4b13bf22cf8e
SZ姉貴の鎖鋸 <131784806+SZanekinokusarinoko@users.noreply.github.com>
2025-08-30 21:28:32 -0500

    Potentially fix pip bugs

--- osint-mint.sh
+++ osint-mint.sh
@@ -6,7 +6,7 @@ sudo apt install git python3-venv python3-pip
 python3 -m venv ~/.env
 source ~/.env/bin/activate
 python3 -m pip install --upgrade pip
-pip3 install pipx xeuledoc ignorant holehe sherlock-project
+python3 -m pip install pipx xeuledoc ignorant holehe sherlock-project
 pipx ensurepath && pipx install ghunt
 
 git clone https://github.com/p1ngul1n0/blackbird.git
